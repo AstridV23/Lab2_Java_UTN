d7223c1b527c494a6e18906c2177177e38a8275d
AstridV23 <astrid@gmail.com>
2023-08-11 12:09:23 -0300

    ordenar numeros

--- src/hola_mundo.java
+++ src/hola_mundo.java
@@ -1,5 +1,3 @@
-package primer_trabajo;
-
 public class hola_mundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!!! :D");
---
+++ src/ordenar_num.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class ordenar_num {
+
+    /*
+    Dado un conjunto de tres números ingresados por el usuario a través de la consola,
+    el programa debe ordenarlos de mayor a menor y mostrar el resultado por pantalla.
+    */
+
+    public static void main(String[] args) {
+        Scanner imput = new Scanner(System.in);
+        int num1, num2, num3;
+        System.out.println("Ingrese 3 números: ");
+        System.out.println("Primer número: ");
+        num1 = imput.nextInt();
+        System.out.println("Segundo número: ");
+        num2 = imput.nextInt();
+        System.out.println("Tercer número: ");
+        num3 = imput.nextInt();
+
+        if(num1 > num2 && num1 > num3){
+            if(num2 > num3){
+                System.out.println("Números ordenados: " +num1+" "+num2+" "+num3);
+            }else{
+                System.out.println("Números ordenados: " +num1+" "+num3+" "+num2);
+            }
+        } else if (num2 > num1 && num2 > num3) {
+            if(num1 > num3){
+                System.out.println("Números ordenados: " +num2+" "+num1+" "+num3);
+            }else{
+                System.out.println("Números ordenados: " +num2+" "+num3+" "+num1);
+            }
+        }else {
+            if(num1 > num2){
+                System.out.println("Números ordenados: " +num3+" "+num1+" "+num2);
+            }else{
+                System.out.println("Números ordenados: " +num3+" "+num2+" "+num1);
+            }
+        }
+
+    }
+}
