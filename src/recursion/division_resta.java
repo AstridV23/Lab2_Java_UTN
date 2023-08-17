0099725053e4bfd18b28bf5124de073067a93014
AstridV23 <astrid@gmail.com>
2023-08-17 19:13:54 -0300

    recursividad actualizado

--- src/recursion/division_resta.java
+++ src/recursion/division_resta.java
@@ -3,21 +3,30 @@ package recursion;
 import java.util.Scanner;
 
 /*
-    Forma recursiva
     Desarrolla un programa que realice la división de dos números enteros
     utilizando tanto el enfoque recursivo como el iterativo con restas sucesivas.
     Implementa ambos enfoques en la misma clase, utilizando la sobrecarga de métodos
     para diferenciarlos. Luego, realiza pruebas utilizando distintos pares de números.
 */
-public class division_resta_recursion {
-    public static int divide(int dend, int isor) {
+public class division_resta {
+    public static int dividirRecursion(int dend, int isor) {
         if (dend == 0) {
             return 0;
         } else if (dend < isor) {
             return 0;
         } else {
-            return 1 + divide(dend - isor, isor);
+            return 1 + dividirRecursion(dend - isor, isor);
         }
+    }
+    public static int dividirIteracion(int dend, int isor) {
+        int cociente = 0;
+
+        while (dend >= isor) {
+            dend -= isor;
+            cociente++;
+        }
+
+        return cociente;
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -27,8 +36,12 @@ public class division_resta_recursion {
         System.out.println("Divisor: ");
         isor = input.nextInt();
 
-        int cociente = divide(dend, isor);
+        int cocienteRec = dividirRecursion(dend, isor);
+        int cocienteIte = dividirIteracion(dend, isor);
 
-        System.out.println("cociente: " + cociente);
+        System.out.println("Recursividad: ");
+        System.out.println("cociente: " + cocienteRec);
+        System.out.println("Iteración: ");
+        System.out.println("cociente: " + cocienteIte);
     }
 }
